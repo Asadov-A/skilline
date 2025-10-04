5b7cd7a6ad10af2d9b1fb102eec8b42681498554
abdu1oh1234 <jabdulloh457@gmail.com>
2025-10-04 17:33:55 +0500

    sfggs

--- src/components/main/section-8/Section8.jsx
+++ src/components/main/section-8/Section8.jsx
@@ -5,6 +5,20 @@ function Section8() {
 
   return (
     <>
+         <main className="main2">
+        <div className="chap3">
+          <img src="/src/assets/icons/rasm2.png" alt="" />
+        </div>
+        <div className="ong3">
+          <p className="ong4">
+            One-on-One <span style="color: #F48C06">Discussions</span>
+          </p>
+          <p className="ong5">
+            Teachers and teacher assistants can talk with students privately
+            without leaving the Zoom environment.
+          </p>
+        </div>
+      </main>
     </>
   )
 }
